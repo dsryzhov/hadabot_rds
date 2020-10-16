a7ba18ad2b5c3a4cba5b2873f3bbe9f622a101ab
dsryzhov <dsryzhov@gmail.com>
2020-10-16 22:56:57 +0300

    Updated docker image for building microros firmware

--- hadabot_microros_firmware/build_in_container.bash
+++ hadabot_microros_firmware/build_in_container.bash
@@ -1,2 +1,2 @@
 #/bin/bash
-docker run -it --rm -v  $(pwd):$(pwd) --workdir $(pwd) dsryzhov/esp-idf-microros /bin/bash -c 'pip3 install catkin_pkg lark-parser empy && idf.py build &&'
+docker run -it --rm --user espidf --volume="/etc/timezone:/etc/timezone:ro" -v  $(pwd):$(pwd) --workdir $(pwd) dsryzhov/esp-idf-microros:latest /bin/bash  -c "idf.py build &&"
