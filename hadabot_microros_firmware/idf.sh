b03d9bf4c3eb983e8d6dd963ca1806cdef23ae5c
dsryzhov <dsryzhov@gmail.com>
2020-10-31 08:42:42 +0300

    -v  /dev:/dev

--- hadabot_microros_firmware/idf.sh
+++ hadabot_microros_firmware/idf.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-docker run -it --rm --user espidf --volume="/etc/timezone:/etc/timezone:ro" -v  $(pwd):$(pwd) --workdir $(pwd) --env IDF_CMD=$1 dsryzhov/esp-idf-microros:idf-v4.0-arduino /bin/bash -c 'echo Running command idf.py $IDF_CMD && idf.py $IDF_CMD ' 
+docker run -it --rm --user espidf --volume="/etc/timezone:/etc/timezone:ro" -v  $(pwd):$(pwd) -v  /dev:/dev --workdir $(pwd) --env IDF_CMD=$1 dsryzhov/esp-idf-microros:idf-v4.0-arduino /bin/bash -c 'echo Running command idf.py $IDF_CMD && idf.py $IDF_CMD ' 
